4507eef3b9f3d46bacc330c837149001cbdb334f
nickmille96 <122224739+nickmille96@users.noreply.github.com>
2023-06-01 18:15:03 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -31,17 +31,100 @@ session= Session(engine)
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
-
-
-#################################################
-# Flask Routes
-#################################################
 @app.route("/")
-def about ():
-    routes = /api/v1.0/precipitation
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+    )
+#Temp Route
+@app.route("/api/v1.0/precipitation")
+def precipation():
+  
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-return routes
+    
+    pre_year= dt.date(2017,8,23)- dt.timedelta(days=365)
+    results =session.query(measurement.date, measurement.prcp).filter(measurement.date >= pre_year).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    precipation = list(np.ravel(results))
+
+    return jsonify(precipation)
+#Stations Route.
+@app.route("/api/v1.0/stations")
+def Station ():
+  
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    statations =session.query(measurement.station, func.count(measurement.station)).group_by(
+    measurement.station).order_by(func.count(measurement.station).desc()).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    Station= list(np.ravel(statations))
+
+    return jsonify(Station)
+
+#Query the dates and temperature observations of the most-active station for the previous year of data.
+@app.route("/api/v1.0/tobs")
+def tobs ():
+  
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    pre_year= dt.date(2017,8,23)- dt.timedelta(days=365)
+    lastyear_temp= session.query(measurement.station, measurement.tobs).\
+    filter(measurement.date >= '2016-08-23')\
+    .filter(measurement.station == 'USC00519281')\
+    .all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    tobs= list(np.ravel(lastyear_temp))
+
+    return jsonify(tobs)
+
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+@app.route("/api/v1.0/<start>", defaults = {"end" : None})
+@app.route("/api/v1.0/<start>/<end>")
+
+def tempreturn ():
+
+    session = Session(engine)
+
+    tempr= [func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
+
+#With Start and end date 
+    if end !=None:
+       statusr= session.query(*tempr).filter(measurement.date >=start).\
+        filter(measurement.date <= end).all()
+       temp= list(np.ravel(statusr))
+    
+    else:
+        statusr= session.query(*tempr).filter(measurement.date >=start).\
+        filter(measurement.date <= end).all()
+
+    session.close()
+
+# Convert into list
+
+Tlist= []
+tdata= False
+for TMIN, TAVG, TMAX in tdata:
+    if TMIN == None or TAVG == None or TMAX ==None:
+        tdata= True
+    Tlist.append(TMIN)
+    Tlist.append(TAVG)
+    Tlist.append(TMAX)
+
 
 
 
